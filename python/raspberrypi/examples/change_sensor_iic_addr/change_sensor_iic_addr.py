bdf68382651cf2de680340cbb4ac88cda77cce0a
Frances9 <44355133+Frances9@users.noreply.github.com>
2021-11-19 09:55:35 +0800

    Update change_sensor_iic_addr.py

--- python/raspberrypi/examples/change_sensor_iic_addr/change_sensor_iic_addr.py
+++ python/raspberrypi/examples/change_sensor_iic_addr/change_sensor_iic_addr.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*
 '''
   @file  change_sensor_iic_addr.py
-  @brief 修改iic地址组
-  @n 实验方式： 将传感器通信引脚与主控连接，烧录
-  @n 通信方式选择，拨码开关SEL：0：IIC,1：UART
-  @n 组序号         组内地址
-  @n A0 A1拨码电平 00    01    10    11
+  @brief Change i2c address set
+  @n Experimental mode: connect sensor communication pin to the main controller and burn
+  @n Communication mode select, dip switch SEL: 0: I2C, 1: UART
+  @n Set serial number         address in the set
+  @n A0 A1 DIP level 00    01    10    11
   @n 1            0x60  0x61  0x62  0x63
   @n 2            0x64  0x65  0x66  0x67
   @n 3            0x68  0x69  0x6A  0x6B
   @n 4            0x6C  0x6D  0x6E  0x6F
   @n 5            0x70  0x71  0x72  0x73
-  @n 6（默认地址组） 0x74  0x75  0x76  0x77（默认地址）
+  @n 6 (Default address set) 0x74  0x75  0x76  0x77 (Default address)
   @n 7            0x78  0x79  0x7A  0x7B
   @n 8            0x7C  0x7D  0x7E  0x7F
-  @n i2c 地址选择，默认i2c地址为0x77，A1、A0组合成4种IIC地址
+  @n i2c address select, default i2c address is 0x77, A1 and A0 are grouped into 4 I2C addresses.
   @n             | A0 | A1 |
   @n             | 0  | 0  |    0x74
   @n             | 0  | 1  |    0x75
   @n             | 1  | 0  |    0x76
   @n             | 1  | 1  |    0x77   default i2c address   
-  @n 实验现象： 串口打印全部数据
+  @n Experimental phenomenon: serial port will print all the data
   @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
   @license     The MIT License (MIT)
   @author      PengKaixing(kaixing.peng@dfrobot.com)
@@ -42,14 +42,14 @@ from DFRobot_MultiGasSensor import *
 ctype=1
 
 if ctype==0:
-  I2C_1       = 0x01               # I2C_1 使用i2c1接口驱动传感器， 可以调整为i2c0但是需要配置树莓派的文件
-  I2C_ADDRESS = 0x77               # I2C 设备的地址，可以更改A1、A0来更换地址，默认地址为0x77
+  I2C_1       = 0x01               # I2C_1 Use i2c1 interface (or i2c0 with configuring Raspberry Pi) to drive sensor
+  I2C_ADDRESS = 0x77               # I2C Device address, which can be changed by changing A1 and A0, the default address is 0x77
   gas = DFRobot_MultiGasSensor_I2C(I2C_1 ,I2C_ADDRESS)
 else:
   gas = DFRobot_MultiGasSensor_UART(9600)
 
 def setup():
-  #修改iic地址组
+  #Change i2c address set
   while(gas.change_i2c_addr_group(6)==0):
     print ("IIC addr change fail!")
     time.sleep(1)
