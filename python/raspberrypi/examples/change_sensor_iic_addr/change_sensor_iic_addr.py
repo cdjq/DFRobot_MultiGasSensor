55f66eb1ee06f2edb9300015808736ff33b024e6
Frances9 <44355133+Frances9@users.noreply.github.com>
2021-11-22 10:24:03 +0800

    Update change_sensor_iic_addr.py

--- python/raspberrypi/examples/change_sensor_iic_addr/change_sensor_iic_addr.py
+++ python/raspberrypi/examples/change_sensor_iic_addr/change_sensor_iic_addr.py
@@ -3,24 +3,24 @@
   @file  change_sensor_iic_addr.py
   @brief Change i2c address set
   @n Experimental mode: connect sensor communication pin to the main controller and burn
-  @n Communication mode select, dip switch SEL: 0: I2C, 1: UART
-  @n Set serial number         address in the set
+  @n Communication mode select, DIP switch SEL: 0: I2C, 1: UART
+  @n Group serial number         Address in the group
   @n A0 A1 DIP level 00    01    10    11
   @n 1            0x60  0x61  0x62  0x63
   @n 2            0x64  0x65  0x66  0x67
   @n 3            0x68  0x69  0x6A  0x6B
   @n 4            0x6C  0x6D  0x6E  0x6F
   @n 5            0x70  0x71  0x72  0x73
-  @n 6 (Default address set) 0x74  0x75  0x76  0x77 (Default address)
+  @n 6 (Default address group) 0x74  0x75  0x76  0x77 (Default address)
   @n 7            0x78  0x79  0x7A  0x7B
   @n 8            0x7C  0x7D  0x7E  0x7F
-  @n i2c address select, default i2c address is 0x77, A1 and A0 are grouped into 4 I2C addresses.
+  @n i2c address select, default to 0x77, A1 and A0 are grouped into 4 I2C addresses.
   @n             | A0 | A1 |
   @n             | 0  | 0  |    0x74
   @n             | 0  | 1  |    0x75
   @n             | 1  | 0  |    0x76
   @n             | 1  | 1  |    0x77   default i2c address   
-  @n Experimental phenomenon: serial port will print all the data
+  @n Experimental phenomenon: serial print all the data
   @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
   @license     The MIT License (MIT)
   @author      PengKaixing(kaixing.peng@dfrobot.com)
